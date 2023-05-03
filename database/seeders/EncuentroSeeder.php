997e23593c206fe87d90dffedc2858956d6429d6
AldoDanielCM <Aldodaniel24@hotmail.com>
2023-05-02 19:06:49 -0600

    SEEDERS

--- database/seeders/EncuentroSeeder.php
+++ database/seeders/EncuentroSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Encuentro;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,40 @@ class EncuentroSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Encuentro::create([
+            'nombre' => 'punto de encuentro 1',
+            'descripcion' => 'Edificio de Gobierno',
+            'latitud' => '19.328857911610612',
+            'longitud' => '-99.11092798763877',
+            'tipo' => 'A pie'
+        ]);
+        Encuentro::create([
+            'nombre' => 'punto de encuentro 2',
+            'descripcion' => 'Salida Av. Santa Ana',
+            'latitud' => ' 19.328517007701326',
+            'longitud' => '--99.11286010122016',
+            'tipo' => 'A pie'
+        ]);
+        Encuentro::create([
+            'nombre' => 'punto de encuentro 1',
+            'descripcion' => 'Edificio de Gobierno',
+            'latitud' => '19.328857911610612',
+            'longitud' => '-99.11092798763877',
+            'tipo' => 'A pie'
+        ]);
+        Encuentro::create([
+            'nombre' => 'punto de encuentro 1',
+            'descripcion' => 'Edificio de Gobierno',
+            'latitud' => '19.328857911610612',
+            'longitud' => '-99.11092798763877',
+            'tipo' => 'A pie'
+        ]);
+        Encuentro::create([
+            'nombre' => 'punto de encuentro 1',
+            'descripcion' => 'Edificio de Gobierno',
+            'latitud' => '19.328857911610612',
+            'longitud' => '-99.11092798763877',
+            'tipo' => 'A pie'
+        ]);
     }
 }
